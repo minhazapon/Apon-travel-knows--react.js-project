861736de91d925aa7478322fd098a66c17687e96
minhazapon <minhajulabedinapon@gmail.com>
2024-04-18 03:14:23 -0700

    news file

--- src/Destination.jsx
+++ src/Destination.jsx
@@ -20,16 +20,16 @@ const Destination = () => {
 
             <section className="py-6  text-gray-50">
 	<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
-		<img src="https://source.unsplash.com/random/301x301/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 bg-gray-500 aspect-square" />
+		<img src="https://hips.hearstapps.com/hbu.h-cdn.co/assets/17/23/bluebell-forest-england.jpg?crop=1.0xw:1xh;center,top&resize=980:*" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 bg-gray-500 aspect-square" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square  " src="https://hips.hearstapps.com/hmg-prod/images/golden-rice-terraces-in-north-vietnam-royalty-free-image-1570205838.jpg?crop=0.447xw:1.00xh;0.335xw,0&resize=980:*" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?1" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?2" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?3" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?4" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?5" />
-     		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?6" />
-     		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?7" />
-     		<img src="https://source.unsplash.com/random/302x302/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 bg-gray-500 aspect-square" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hmg-prod/images/early-morning-in-a-provences-lavender-field-with-a-royalty-free-image-1570211404.jpg?crop=0.447xw:1.00xh;0.456xw,0&resize=980:*" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hmg-prod/images/deadvlei-trees-with-the-colorful-background-in-royalty-free-image-1588101586.jpg?crop=1xw:0.99953xh;center,top&resize=980:*" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hmg-prod/images/scenic-view-of-neuschwanstein-castle-in-germany-royalty-free-image-1570209362.jpg?crop=0.447xw:1.00xh;0.214xw,0&resize=980:*" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hmg-prod/images/aerial-view-of-bora-bora-french-polynesia-royalty-free-image-1570212797.jpg?crop=0.502xw:1.00xh;0.354xw,0&resize=980:*" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hbu.h-cdn.co/assets/16/26/blue-ridge-parkway.jpg?crop=1xw:0.9998181818181818xh;center,top&resize=980:*" />
+     	<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hbu.h-cdn.co/assets/17/23/1600x2409/antelope-canyon-arizona.jpg?resize=980:*" />
+     	<img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://hips.hearstapps.com/hbu.h-cdn.co/assets/17/23/bluebell-forest-england.jpg?crop=1.0xw:1xh;center,top&resize=980:*" />
+     	<img src="https://hips.hearstapps.com/hmg-prod/images/coatline-of-zanzibar-at-the-indian-ocean-royalty-free-image-1570219116.jpg?crop=0.318xw:1.00xh;0.0595xw,0&resize=980:*" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 bg-gray-500 aspect-square" />
      	</div>
      </section>
 
--- src/News.jsx
+++ src/News.jsx
@@ -3,8 +3,142 @@
 
 
 const News = () => {
-    return (
-        <div>
+       return (
+           <div className=" mt-10 mb-10 mr-10 ml-10">
+   
+               <div>
+   
+                            
+
+
+               <div className="min-h-screen  rounded-xl text-gray-100">
+	<div className="p-6 space-y-8">
+		
+		<main>
+			<div className="container mx-auto space-y-16">
+				<section className="grid gap-6 text-center lg:grid-cols-2 xl:grid-cols-5">
+					<div className="w-full p-6 rounded-md sm:p-16 xl:col-span-2 bg-[#135D66]">
+						<span className="block mb-2 text-violet-400">Apon travel News</span>
+						<h1 className="text-5xl font-extrabold text-gray-50">Build News with Apon travel</h1>
+						<p className="my-8">
+							<span className="font-medium text-gray-50"> Join For Apon Travel News and journalism </span>
+						</p>
+						<form noValidate="" action="" className="self-stretch space-y-3">
+							<div>
+								<label htmlFor="name" className="text-sm sr-only">Your name</label>
+								<input id="name" type="text" placeholder="Your name" className="  p-3  w-full rounded-md focus:ring focus:ring-violet-400 border-gray-700" />
+							</div>
+							<div>
+								<label htmlFor="lastname" className="text-sm sr-only">Email address</label>
+								<input id="lastname" type="text" placeholder="Email address" className=" p-3  w-full rounded-md focus:ring focus:ring-violet-400 border-gray-700" />
+							</div>
+							<button className=" btn  w-full py-2 font-semibold rounded bg-[#135D66] text-white">Join the journalist Team</button>
+						</form>
+					</div>
+					<img src="https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1500181969-restricted.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp" alt="" className="object-cover w-full h-full rounded-md xl:col-span-3 bg-gray-500" />
+				</section>
+				<section>
+					<span className="block mb-2 text-xs font-medium tracking-widest uppercase lg:text-center text-violet-400">How it works</span>
+					<h2 className="text-5xl font-bold lg:text-center text-[#135D66]">Build News With Our Travel team</h2>
+					<div className="grid gap-6 my-16 lg:grid-cols-3">
+						<div className="flex flex-col p-8 space-y-4 rounded-md bg-gray-900">
+							<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-violet-400 text-gray-900">1</div>
+							<p className="text-2xl font-semibold">
+								<b>Nulla.</b>Nostrum, corrupti blanditiis. Illum, architecto?
+							</p>
+						</div>
+						<div className="flex flex-col p-8 space-y-4 rounded-md bg-gray-900">
+							<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-violet-400 text-gray-900">2</div>
+							<p className="text-2xl font-semibold">
+								<b>Accusantium.</b>Vitae saepe atque neque sunt eius dolor veniam alias debitis?
+							</p>
+						</div>
+						<div className="flex flex-col p-8 space-y-4 rounded-md bg-gray-900">
+							<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-violet-400 text-gray-900">3</div>
+							<p className="text-2xl font-semibold">
+								<b>Maxime.</b>Expedita temporibus culpa reprehenderit doloribus consectetur odio!
+							</p>
+						</div>
+					</div>
+				</section>
+				<section className="grid gap-6 lg:grid-cols-2">
+					<img src="https://source.unsplash.com/random/360x480" alt="" className="object-cover w-full rounded-md max-h-96 bg-gray-500" />
+					<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 bg-gray-900">
+						<img src="https://source.unsplash.com/random/100x100" alt="" className="object-cover w-20 h-20 rounded-full bg-gray-500" />
+						<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+						<div className="text-center text-gray-400">
+							<p>Leroy Jenkins</p>
+							<p>CEO of Company Co.</p>
+						</div>
+						<div className="flex space-x-2">
+							<button type="button" aria-label="Page 1" className="w-2 h-2 rounded-full bg-gray-50"></button>
+							<button type="button" aria-label="Page 2" className="w-2 h-2 rounded-full bg-gray-600"></button>
+							<button type="button" aria-label="Page 3" className="w-2 h-2 rounded-full bg-gray-600"></button>
+							<button type="button" aria-label="Page 4" className="w-2 h-2 rounded-full bg-gray-600"></button>
+						</div>
+					</div>
+					<div className="p-8 space-y-8 rounded-md lg:col-span-full lg:py-12 bg-gray-900">
+						<h2 className="text-5xl font-bold text-gray-50">Create with us</h2>
+						<p className="text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt facilis quod accusantium beatae cum nam adipisci reiciendis omnis possimus error quo animi voluptas magni, at incidunt. Nulla ex at ullam corporis quidem adipisci vitae, perferendis dolorem libero minus atque tenetur enim pariatur cupiditate commodi in beatae, ipsa eligendi? Quis, saepe.</p>
+					</div>
+				</section>
+				<section>
+					<div className="grid gap-6 lg:grid-cols-3">
+						<div className="overflow-hidden rounded lg:flex lg:col-span-3">
+							<img src="https://source.unsplash.com/random/485x365" alt="" className="object-cover w-full h-auto max-h-96 bg-gray-500" />
+							<div className="p-6 space-y-6 lg:p-8 md:flex md:flex-col bg-gray-900">
+								<span className="self-start px-3 py-1 text-sm rounded-full bg-violet-400 text-gray-900">Business</span>
+								<h2 className="text-3xl font-bold md:flex-1">Curating a workplace that inspires team movement</h2>
+								<div>
+									<p className="text-gray-400">November 30, 2020</p>
+									<p className="text-gray-400">By Leroy Jenkins</p>
+								</div>
+							</div>
+						</div>
+						<div className="p-6 rounded lg:p-8 lg:py-12 bg-gray-900">
+							<h3 className="inline font-medium text-gray-50">Panel-based blocks.</h3>
+							<p className="inline">Flexible panels that are perfect for building functional layouts.</p>
+						</div>
+						<div className="p-6 rounded lg:p-8 lg:py-12 bg-gray-900">
+							<h3 className="inline font-medium text-gray-50">Responsive design.</h3>
+							<p className="inline">Panels look great no matter the device.</p>
+						</div>
+						<div className="p-6 rounded lg:p-8 lg:py-12 bg-gray-900">
+							<h3 className="inline font-medium text-gray-50">Premium support.</h3>
+							<p className="inline">Join over 50 000 satisfied customers who use our templates.</p>
+						</div>
+					</div>
+				</section>
+				<section>
+					<div className="container p-6 py-20 mx-auto rounded-xl lg:px-8 bg-[#135D66]">
+						<h2 className="text-5xl font-bold text-center">Our team Apon Travel News is here to help you.</h2>
+						<div className="flex justify-center p-4">
+							<a rel="noopener noreferrer" href="#">Join Our Team</a>
+						</div>
+						<img src="https://media.licdn.com/dms/image/D5612AQF3TekgxJSlJA/article-cover_image-shrink_720_1280/0/1701980104569?e=2147483647&v=beta&t=NMqOt-U00wD_ndbzB0Vvtz4Dufa5o6ItE01I3zpHuGg" alt="" className="object-cover w-full h-auto mt-8 rounded max-h-96 bg-gray-500" />
+					</div>
+				</section>
+			</div>
+		</main>
+		
+	</div>
+</div>
+
+{/* --------------------------- */}
+
+
+       
+
+
+
+
+
+
+
+
+
+
+            </div>
             
         </div>
     );
